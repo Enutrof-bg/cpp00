213d8dc22863e38bfddc064818f8628df329652f
Kevin Wang <kevwang@f6r11s15.clusters.42paris.fr>
2025-07-09 16:33:21 +0200

    up

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -10,4 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+using namespace std;
 
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	if (argc >= 2)
+	{
+		int i = 0;
+		int j = 1;
+		while (j < argc)
+		{
+			while (argv[j][i])
+			{
+				if (argv[j][i] >= 'a' && argv[j][i] <= 'z')
+					argv[j][i] = argv[j][i] - 32;
+				i++;
+			}
+			i = 0;
+			j++;
+		}
+		int n = 1;
+		while (n < argc)
+		{
+			cout << argv[n];
+			n++;
+		}
+		cout << "\n";
+	}
+		return 0;
+}
