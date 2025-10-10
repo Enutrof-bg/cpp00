5e19eab5097b0dc64d2b949e7078f9e214cdf688
Kevin Wang <kevwang@f6r11s16.clusters.42paris.fr>
2025-10-10 13:42:34 +0200

    up

---
+++ cpp01/ex03/Weapon.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevwang <kevwang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/10 13:32:18 by kevwang           #+#    #+#             */
+/*   Updated: 2025/10/10 13:32:19 by kevwang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Weapon.hpp"
+
+std::string Weapon::getType()
+{
+	return type;
+}
+
+void Weapon::setType(std::string newType)
+{
+	type = newType;
+}
---
+++ cpp01/ex03/Weapon.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevwang <kevwang@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/10 13:32:14 by kevwang           #+#    #+#             */
+/*   Updated: 2025/10/10 13:32:15 by kevwang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
+
+# include <iostream>
+# include <stdio.h>
+# include <limits>
+# include <string>
+
+class Weapon
+{
+private:
+	std::string type;
+
+public:
+	std::string getType(void);
+	void setType(std::string newType);
+};
+
+#endif
+
