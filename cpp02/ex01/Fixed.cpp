d3572b7c0c2f3dffab796e5cb7ed3fce580c1620
Kevin Wang <kevwang@f6r10s13.clusters.42paris.fr>
2025-10-17 11:40:30 +0200

    up

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -45,8 +45,7 @@ Fixed &Fixed::operator=(const Fixed &other)
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other)
 	{
-		// _fixed = other.getRawBits();
-		_fixed = other._fixed;
+		_fixed = other.getRawBits();
 	}
 	return *this;
 }
